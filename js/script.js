8770712958e08fd71534f419944cb92cc8c31f91
Alexander Cole Mora <alexandercolemora@gmail.com>
2017-10-23 19:34:01 +0100

    trabajando en el menu

--- js/script.js
+++ js/script.js
@@ -1,14 +1,12 @@
-$(document).ready(main);
+$(document).ready(function(){
+	var contador = 1;
 
-var contador = 1;
-
-function main(){
 	$('.menu_bar').click(function(){
 		// $('nav').toggle();
 
 		if(contador == 1){
 			$('nav').animate({
-				left: '0'
+				left: '0',
 			});
 			contador = 0;
 		} else {
@@ -17,7 +15,24 @@ function main(){
 				left: '-100%'
 			});
 		}
+	});
+
+	$(window).resize(function(){
+		if($(window).width() > 900){
+			$('nav').animate({
+				left: '0'
+			});
+			document.getElementById('nombre').style.display="block";
+		}
+		else {
+			$('nav').animate({
+				left: '-100%'
+			});
+			document.getElementById('nombre').style.display="none";
+		}
 
 	});
 
-};
+});
+
+//style="display:none"
